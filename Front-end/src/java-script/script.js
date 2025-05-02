5a956dcc8ca9c8bf3df20b0efb9614cb9714d4d6
KelsonZh0 <kel180498@gmail.com>
2025-05-02 18:11:14 -0300

    atulizção na parte de itegrantes

--- Front-end/src/java-script/script.js
+++ Front-end/src/java-script/script.js
@@ -1,16 +1,24 @@
-const cards = document.querySelectorAll('.cartao-integrante');
-const botao = document.getElementById('btn-proximo');
-let indiceAtual = 0;
 
-function mostrarCard(indice) {
-  cards.forEach((card, i) => {
-    card.classList.toggle('ativo', i === indice);
-  });
+
+let indiceAtual = 0;
+const cartoes = document.querySelectorAll('.cartao-integrante');
+const botaoProximo = document.getElementById('btn-proximo');
+
+function mostrarCartao(indice) {
+  cartoes.forEach(cartao => cartao.classList.remove('ativo'));
+  cartoes[indice].classList.add('ativo');
 }
 
-botao.addEventListener('click', () => {
-  indiceAtual = (indiceAtual + 1) % cards.length;
-  mostrarCard(indiceAtual);
-});
+function proximoCartao() {
+  indiceAtual = (indiceAtual + 1) % cartoes.length;
+  mostrarCartao(indiceAtual);
+}
 
-mostrarCard(indiceAtual); // inicia com o primeiro card
+// Iniciar com o primeiro cartão visível
+mostrarCartao(indiceAtual);
+
+// Alternar automaticamente a cada 5 segundos
+setInterval(proximoCartao, 5000);
+
+// Alternar manualmente com botão
+botaoProximo.addEventListener('click', proximoCartao);
